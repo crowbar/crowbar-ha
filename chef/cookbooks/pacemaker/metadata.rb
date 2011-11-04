d2ecca5edd71bab6e632acaf6573733227396878
mattray <matthewhray@gmail.com>
2011-11-04 14:33:36 -0500

    point to readme.md

--- chef/cookbooks/pacemaker/metadata.rb
+++ chef/cookbooks/pacemaker/metadata.rb
@@ -2,5 +2,5 @@ maintainer       "Keith Hudgins"
 maintainer_email "keith@dtosolutions.com"
 license          "Apache 2.0"
 description      "Installs/Configures Pacemaker/Corosync"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.me'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
