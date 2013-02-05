0773628dc3e5b3df2958e7203e197286e9ffc8a6
rackerjoe <breu@breu.org>
2013-02-05 12:54:54 -0600

    fix metadata for chef11 server

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ license          "Apache 2.0"
 description      "Installs and configures a base corosync installation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.2"
-provides	 "service[corosync]"
 
 %w{ ubuntu fedora }.each do |os|
   supports os
