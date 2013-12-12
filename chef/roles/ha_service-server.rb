1168b3556957c70cc182c4481db5b1406840a779
Adam Spiers <aspiers@suse.com>
2013-12-12 21:13:24 +0000

    remove "fat" roles
    Defining attributes and long run lists in roles are both anti-patterns:
    
      http://dougireton.com/blog/2013/02/16/chef-cookbook-anti-patterns/

--- chef/roles/ha_service-server.rb
+++
@@ -1,9 +0,0 @@
-name "ha_service-server"
-description "Ha service Server Role"
-run_list(
-         "recipe[ha_service::api]",
-         "recipe[ha_service::monitor]"
-)
-default_attributes()
-override_attributes()
-
