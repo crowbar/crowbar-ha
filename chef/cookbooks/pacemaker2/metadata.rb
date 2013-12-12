d4242e312eaff5fd0aa0b7b1af2895d7f7962e0a
Adam Spiers <aspiers@suse.com>
2013-12-12 22:59:32 +0000

    merge pacemaker cookbooks
    Preliminary merge of git://github.com/ktc-itsnotbutter/pacemaker.git
    (in the chef/cookbooks/pacemaker/ directory) with the cookbook
    from the pacemaker_service branch of:
    
      https://github.com/mattray/barclamp_ha_service.git
    
    (in the chef/cookbooks/pacemaker2/ directory)
    
    since both forks had some nice stuff.  This merge is not complete yet.

--- chef/cookbooks/pacemaker2/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Crowbar Project"
-maintainer_email "crowbar@dell.com"
-license          "Apache 2.0"
-description      "Installs/configures Pacemaker"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1"
