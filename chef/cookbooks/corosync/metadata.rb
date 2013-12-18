95e200feeadb6e121d9a1c9683dccd053830f6a8
Adam Spiers <aspiers@suse.com>
2013-12-18 17:30:22 +0000

    can't realistically claim to support Ubuntu or Fedora yet
    since code hasn't been tested on them yet.

--- chef/cookbooks/corosync/metadata.rb
+++ chef/cookbooks/corosync/metadata.rb
@@ -4,6 +4,6 @@ description      "Installs and configures a base corosync installation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.1.0"
 
-%w{ ubuntu fedora suse }.each do |os|
+%w(suse).each do |os|
   supports os
 end
