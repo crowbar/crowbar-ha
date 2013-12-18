256ea3576ad40e87f44dfafcb0628090d6a9bde8
Adam Spiers <aspiers@suse.com>
2013-12-18 17:23:31 +0000

    bump version to 1.1.0
    Hopefully shouldn't have broken anything which wasn't already broken ;-)
    And certainly added some features.

--- chef/cookbooks/corosync/metadata.rb
+++ chef/cookbooks/corosync/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "SUSE, GmbH"
 license          "Apache 2.0"
 description      "Installs and configures a base corosync installation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.5"
+version          "1.1.0"
 
 %w{ ubuntu fedora suse }.each do |os|
   supports os
