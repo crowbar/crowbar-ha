bc6f94446565ccc34fab6bc05f6dca647710060d
Vincent Untz <vuntz@suse.com>
2014-02-24 14:55:59 +0100

    Assume that repos are missing if attribute is not set

--- crowbar_framework/app/models/pacemaker_service.rb
+++ crowbar_framework/app/models/pacemaker_service.rb
@@ -84,7 +84,7 @@ class PacemakerService < ServiceObject
     unless nodes.nil? or nodes.length < 1
       provisioner_server_node = nodes[0]
       if provisioner_server_node[:platform] == "suse"
-        if (provisioner_server_node[:provisioner][:suse][:missing_hae] rescue false)
+        if (provisioner_server_node[:provisioner][:suse][:missing_hae] rescue true)
           validation_error "The HAE repositories have not been setup."
         end
       end
