853e7ecf1b59f085ca76c7792119e1e3ef336262
Balazs Kutil <bkutil@suse.com>
2014-03-11 08:28:16 +0100

    Add haproxy_servers_for_service helper
    This provides a convenient method for cookbooks that will use
    haproxy_loadbalancer.

--- chef/cookbooks/crowbar-pacemaker/libraries/helpers.rb
+++ chef/cookbooks/crowbar-pacemaker/libraries/helpers.rb
@@ -87,4 +87,75 @@ module CrowbarPacemakerHelper
 
     [haproxy_servers, haproxy_servers_nodes]
   end
+
+  # Each node uses Chef search via #cluster_nodes in order to
+  # determine the list of haproxy backend servers which need to be
+  # included in haproxy.cfg.  In order to minimise the number of Chef
+  # searches and the consequent building of data structures, we
+  # memoize the results in this cache.  It is keyed by (node, role)
+  # tuple and the values are the 2-element Arrays returned by
+  # #haproxy_servers.  (Technically it should be keyed by (cluster,
+  # role), since the results should be the same for any node within
+  # the cluster, but since the cache is not shared and has to be
+  # constructed per node, it should not make any difference, because
+  # calls to #haproxy_servers_for_service and #haproxy_servers on a
+  # single node should never span multiple clusters.)
+  @@haproxy_servers_cache = {}
+
+  # Returns an Array of servers that can be passed as the servers
+  # parameter to the haproxy_loadbalancer LWRP.  It assumes that all
+  # the servers that will be load-balanced are listening on the admin
+  # network.
+  #
+  # Arguments:
+  #   - node
+  #       the current Chef node object (facilitates access to
+  #       relevant attributes)
+  #   - name
+  #       used in conjunction with 'ports_key' parameter to
+  #       obtain the correct port to use for each server, which is stored
+  #       in the server_node[name][:ha][:ports][ports_key] attribute
+  #   - role
+  #       the name of the Chef role managing this service (used
+  #       to ensure the results are memoized per role)
+  #   - ports_key
+  #       used in conjunction with 'name' parameter - see above
+  #
+  # Concrete example:
+  #
+  #   CrowbarPacemakerHelper.haproxy_servers_for_service(node, "glance", "glance-server", "api")
+  #
+  # This would:
+  #   - look for all nodes in the cluster with the glance-server role
+  #   - create a list of servers to be used with the haproxy_loadbalancer
+  #     LWRP, with each found node being an element of the list, with:
+  #     - the admin IP of the node used as the address field
+  #     - the glance.ha.ports.api (where glance and api come from the name
+  #       and ports_key parameters) attribute of the node used as the port
+  #       field
+  def self.haproxy_servers_for_service(node, name, role, ports_key)
+    # Fetch configured HA proxy servers for a given service
+    cache_key = "#{node}-#{role}"
+
+    servers, server_nodes = @@haproxy_servers_cache.fetch(cache_key) do
+      @@haproxy_servers_cache[cache_key] = haproxy_servers(node, role)
+    end
+
+    # Clone each server Hash because we're going to change each of
+    # them, and we don't want to change the cache which is shared
+    # between all callers.  This is because different callers will
+    # use different values for the 'name' parameter, which can result
+    # in different port values.
+    servers = servers.map { |s| s.clone }
+
+    # Look up and store where they are listening
+    servers.each do |server|
+      server_node = server_nodes[server['name']]
+
+      server['address'] = Chef::Recipe::Barclamp::Inventory.get_network_by_type(server_node, "admin").address
+      server['port']    = server_node[name.to_sym][:ha][:ports][ports_key.to_sym]
+    end
+
+    servers
+  end
 end
