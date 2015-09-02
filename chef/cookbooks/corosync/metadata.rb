29e9426949f851cf961bbe97f5b1ce740a515c40
Ovais Tariq <ovaistariq@gmail.com>
2015-09-01 20:05:16 -0700

    Fixes #9

--- chef/cookbooks/corosync/metadata.rb
+++ chef/cookbooks/corosync/metadata.rb
@@ -3,7 +3,7 @@ maintainer       "SUSE, GmbH"
 license          "Apache 2.0"
 description      "Installs and configures a base corosync installation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.1"
+version          "1.1.2"
 
 depends "yum"
 depends "yum-epel", '<= 0.6.0'
