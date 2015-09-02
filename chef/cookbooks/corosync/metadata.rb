e001e294147d7ea5ddaa0e3be9f019fc4c099318
Ovais Tariq <ovaistariq@gmail.com>
2015-09-01 18:05:12 -0700

    - Update cookbook metadata to add dependent cookbooks and add support   for RHEL platform_family - Add the LICENSE file - Add berks and test-kitchen config which are needed for cookbook   testing

--- chef/cookbooks/corosync/metadata.rb
+++ chef/cookbooks/corosync/metadata.rb
@@ -1,10 +1,13 @@
-name "corosync"
-maintainer "SUSE, GmbH"
-license "Apache 2.0"
-description "Installs and configures a base corosync installation"
-long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version "1.1.0"
+name             "corosync"
+maintainer       "SUSE, GmbH"
+license          "Apache 2.0"
+description      "Installs and configures a base corosync installation"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.1.1"
 
-%w(suse).each do |os|
+depends "yum"
+depends "yum-epel", '<= 0.6.0'
+
+%w{ redhat centos suse }.each do |os|
   supports os
 end
