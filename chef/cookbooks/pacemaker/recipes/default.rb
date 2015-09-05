f18a21d816f1f80ea29f8e7b10d03593866ca8e5
Ovais Tariq <ovaistariq@gmail.com>
2015-09-04 18:45:41 -0700

    Add support for proper installation on RHEL platform

--- chef/cookbooks/pacemaker/recipes/default.rb
+++ chef/cookbooks/pacemaker/recipes/default.rb
@@ -1,9 +1,10 @@
 #
-# Author:: Robert Choi
+# Author:: Robert Choi, Ovais Tariq <me@ovaistariq.net>
 # Cookbook Name:: pacemaker
 # Recipe:: default
 #
 # Copyright 2013, Robert Choi
+# Copyright 2015, Ovais Tariq
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,17 +19,49 @@
 # limitations under the License.
 #
 
-if node[:pacemaker][:platform][:packages].nil?
-  Chef::Application.fatal! "FIXME: #{node.platform} platform not supported yet"
+if node["pacemaker"]["platform"]["packages"].nil?
+  Chef::Application.fatal! "FIXME: #{node['platform']} platform not supported yet"
 end
 
-node[:pacemaker][:platform][:packages].each do |pkg|
-  package pkg
+# The crm command-line needs to be installed separately on RHEL
+crmsh_package = node["pacemaker"]["platform"]["crm_package"]
+
+# Configure the necessary yum repositories
+case node["platform_family"]
+when 'rhel'
+  # Configure the opensuse HA/Clustering repository for CentOS 6
+  yum_repository 'network_ha-clustering_Stable' do
+    description "Stable High Availability/Clustering packages (CentOS_CentOS-6)"
+    baseurl "http://download.opensuse.org/repositories/network:/ha-clustering:/Stable/CentOS_CentOS-6/"
+    gpgkey 'http://download.opensuse.org/repositories/network:/ha-clustering:/Stable/CentOS_CentOS-6/repodata/repomd.xml.key'
+    gpgcheck true
+    enabled true
+    action :nothing
+  end
+
+  # Install the crmsh package separately
+  package crmsh_package do
+    action :nothing
+
+    # Disable the opensuse HA/Clustering repository
+    notifies :delete, 'yum_repository[network_ha-clustering_Stable]', :immediately
+  end
+end
+
+node["pacemaker"]["platform"]["packages"].each do |pkg|
+  package pkg do
+    action :install
+
+    if node["platform_family"] == "rhel"
+      notifies :create, 'yum_repository[network_ha-clustering_Stable]', :immediately
+      notifies :install, "package[#{crmsh_package}]", :immediately
+    end
+  end
 end
 
 unless node.platform == "suse" && node.platform_version.to_f >= 12.0
-  if node[:pacemaker][:setup_hb_gui]
-    node[:pacemaker][:platform][:graphical_packages].each do |pkg|
+  if node["pacemaker"]["setup_hb_gui"]
+    node["pacemaker"]["platform"]["graphical_packages"].each do |pkg|
       package pkg
     end
 
@@ -44,6 +77,7 @@ unless node.platform == "suse" && node.platform_version.to_f >= 12.0
   end
 end
 
+# Setup corosync
 if Chef::Config[:solo]
   unless ENV["RSPEC_RUNNING"]
     Chef::Application.fatal! \
@@ -64,12 +98,10 @@ if (platform_family?("suse") && node.platform_version.to_f >= 12.0) || platform_
   else
     enable_or_disable = :enable
   end
-
   service "pacemaker" do
     action [enable_or_disable, :start]
-    if platform_family? "rhel"
-      notifies :restart, "service[clvm]", :immediately
-    end
+    retries 3
+    retry_delay 30
   end
 end
 
@@ -91,7 +123,7 @@ ruby_block "wait for cluster to be online" do
   end # block
 end # ruby_block
 
-if node[:pacemaker][:founder]
+if node["pacemaker"]["founder"]
   include_recipe "pacemaker::setup"
 end
 
