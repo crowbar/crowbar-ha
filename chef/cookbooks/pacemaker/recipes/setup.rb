a2feb19bdd09f9716848003e25463d016cfb8335
Ovais Tariq <ovaistariq@gmail.com>
2015-09-04 21:59:01 -0700

    Use Chef recommended dsl format

--- chef/cookbooks/pacemaker/recipes/setup.rb
+++ chef/cookbooks/pacemaker/recipes/setup.rb
@@ -18,7 +18,7 @@
 # limitations under the License.
 #
 
-crm_conf = node[:pacemaker][:crm][:initial_config_file]
+crm_conf = node['pacemaker']['crm']['initial_config_file']
 
 template crm_conf do
   source "crm-initial.conf.erb"
@@ -26,15 +26,15 @@ template crm_conf do
   group "root"
   mode 0600
   variables(
-    stonith_enabled: (node[:pacemaker][:stonith][:mode] != "disabled"),
-    no_quorum_policy: node[:pacemaker][:crm][:no_quorum_policy],
-    op_default_timeout: node[:pacemaker][:crm][:op_default_timeout]
+    :stonith_enabled => (node['pacemaker']['stonith']['mode'] != "disabled"),
+    :no_quorum_policy => node['pacemaker']['crm']['no_quorum_policy'],
+    :op_default_timeout => node['pacemaker']['crm']['op_default_timeout']
   )
 end
 
 execute "crm initial configuration" do
   user "root"
   command "crm configure load replace #{crm_conf}"
-  subscribes :run, resources(template: crm_conf), :immediately
+  subscribes :run, "template[#{crm_conf}]", :immediately
   action :nothing
 end
