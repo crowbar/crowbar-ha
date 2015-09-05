ac97ded4fb86ce01e61a72ee8bc29177e328b82d
Ovais Tariq <ovaistariq@gmail.com>
2015-09-04 22:59:19 -0700

    Main pacemaker library requires all the custom library modules to bring them in scope

--- chef/cookbooks/pacemaker/libraries/pacemaker.rb
+++ chef/cookbooks/pacemaker/libraries/pacemaker.rb
@@ -1,7 +1,27 @@
-this_dir = File.dirname(__FILE__)
 
-require File.expand_path("pacemaker/resource/primitive",    this_dir)
-require File.expand_path("pacemaker/resource/clone",        this_dir)
-require File.expand_path("pacemaker/resource/ms",           this_dir)
-require File.expand_path("pacemaker/resource/group",        this_dir)
-require File.expand_path("pacemaker/constraint/colocation", this_dir)
+require 'chef/recipe'
+require 'chef/resource'
+require 'chef/provider'
+
+class Chef
+  module Pacemaker
+    require_relative 'cib_object'
+    require_relative 'mixins_resource_meta'
+    require_relative 'standard_cib_object'
+    require_relative 'stonith'
+
+    # Resources
+    require_relative 'resource'
+    require_relative 'resource_clone'
+    require_relative 'resource_group'
+    require_relative 'resource_ms'
+    require_relative 'resource_primitive'
+    require_relative 'runnable_resource'
+
+    # Constraints
+    require_relative 'constraint'
+    require_relative 'constraint_colocation'
+    require_relative 'constraint_location'
+    require_relative 'constraint_order'
+  end
+end
