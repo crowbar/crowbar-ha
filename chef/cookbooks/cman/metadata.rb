fd204c67131ddcfc26797505c9999f96b50ed44d
Vincent Untz <vuntz@suse.com>
2015-10-01 16:04:55 +0200

    Drop cman cookbook
    It's not used, so not sure why we ship it.

--- chef/cookbooks/cman/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer "Rackspace US, Inc"
-license "Apache 2.0"
-description "Installs and configures a base CMAN installation"
-long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version "1.0.5"
-
-%w{ ubuntu fedora }.each do |os|
-  supports os
-end
