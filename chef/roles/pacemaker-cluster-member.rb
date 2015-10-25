61ad653d4f2611ebf53d808d3f4c1e46d4b03683
Vincent Untz <vuntz@suse.com>
2015-10-25 08:26:27 +0100

    Add "role recipes" with the goal of simplifying the roles
    We want the roles to only reference one recipe, so that we can put some
    intelligence in that recipe:
    
     - in a first step, we will stop changing the run list of a node
       depending on its state, and instead have the role recipe "decide"
       whether something should be done or not, depending on the state. This
       will solve the issue that the search results for nodes are not
       reliable when a node is in some state that is not "readying",
       "applying" or "ready".
    
       Very concretely: many recipes are using searches to find attributes
       of another barclamp. The recipes do not expect the search to return
       an empty result (and that's okay, because we know that there must be
       a glance server when we deploy nova, due to barclamp dependencies),
       but that could happen in the past when the node was in "problem" or
       rebooting.
    
     - in a later step, we might want to have a new attribute that will tell
       us which role to apply instead of applying everything.
    
    More generally speaking, it's actually wrong to pretend that a node
    stops having a role because it's, say, rebooting. A glance server is
    always a glance server.

--- chef/roles/pacemaker-cluster-member.rb
+++ chef/roles/pacemaker-cluster-member.rb
@@ -1,7 +1,7 @@
 name "pacemaker-cluster-member"
 description "Pacemaker cluster member"
 run_list(
-         "recipe[crowbar-pacemaker::default]"
+         "recipe[crowbar-pacemaker::role_pacemaker_cluster_member]"
 )
 default_attributes()
 override_attributes()
