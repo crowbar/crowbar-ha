7849dcf17632ce5807bb9fb85e7386c7e953075e
Adam Spiers <aspiers@suse.com>
2015-12-30 19:30:53 +0000

    use rspec's be_true / be_false matchers
    These give better output than be(true) / be(false).

--- chef/cookbooks/pacemaker/spec/libraries/pacemaker/resource_spec.rb
+++ chef/cookbooks/pacemaker/spec/libraries/pacemaker/resource_spec.rb
@@ -19,13 +19,13 @@ describe Pacemaker::Resource do
     it "should return true" do
       expect(@cmd).to receive(:stdout).at_least(:once) \
         .and_return("resource #{rsc.name} is running on: d52-54-00-e5-6b-a0")
-      expect(rsc.running?).to be(true)
+      expect(rsc.running?).to be_true
     end
 
     it "should return false" do
       expect(@cmd).to receive(:stdout).at_least(:once) \
         .and_return("resource #{rsc.name} is NOT running")
-      expect(rsc.running?).to be(false)
+      expect(rsc.running?).to be_false
     end
   end
 
