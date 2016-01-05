6e706f8a7269de87ea25689e998cb73f26e89c8f
Vincent Untz <vuntz@suse.com>
2016-01-05 15:36:56 +0100

    Ensure pacemaker-remote nodes cannot have another role
    The remote nodes cannot be normal cluster members.

--- crowbar_framework/app/models/pacemaker_service.rb
+++ crowbar_framework/app/models/pacemaker_service.rb
@@ -45,7 +45,7 @@ class PacemakerService < ServiceObject
           }
         },
         "pacemaker-remote" => {
-          "unique" => false,
+          "unique" => true,
           "count" => -1,
           "platform" => {
             "suse" => "/.*/",
