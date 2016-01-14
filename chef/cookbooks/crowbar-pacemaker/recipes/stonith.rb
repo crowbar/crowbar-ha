10ca465ded82112447d3e1463ff8d84976fd33d6
Vincent Untz <vuntz@suse.com>
2016-01-15 00:58:55 +0100

    crowbar-pacemaker: Split out non-stonith sbd code to its own recipe
    This can then be run from remote nodes.

--- chef/cookbooks/crowbar-pacemaker/recipes/stonith.rb
+++ chef/cookbooks/crowbar-pacemaker/recipes/stonith.rb
@@ -25,42 +25,7 @@ node[:pacemaker][:stonith][:per_node][:mode] = "self"
 
 case node[:pacemaker][:stonith][:mode]
 when "sbd"
-  sbd_devices = nil
-  sbd_devices ||= (node[:pacemaker][:stonith][:sbd][:nodes][node[:fqdn]][:devices] rescue nil)
-  sbd_devices ||= (node[:pacemaker][:stonith][:sbd][:nodes][node[:hostname]][:devices] rescue nil)
-
-  sbd_devices.each do |sbd_device|
-    if File.symlink?(sbd_device)
-      sbd_device_simple = File.expand_path(File.readlink(sbd_device), File.dirname(sbd_device))
-    else
-      sbd_device_simple = sbd_device
-    end
-    disks = BarclampLibrary::Barclamp::Inventory::Disk.all(node).select { |d| d.name == sbd_device_simple }
-    disk = disks.first
-    if disk.nil?
-      # This is not a disk; let's see if this is a partition and deal with it
-      sbd_sys_dir = "/sys/class/block/#{File.basename(sbd_device_simple)}"
-      if File.exists?("#{sbd_sys_dir}/partition") && File.symlink?(sbd_sys_dir)
-        sbd_sys_dir_full = File.expand_path(File.readlink(sbd_sys_dir), File.dirname(sbd_sys_dir))
-        # sbd_sys_dir_full is something like
-        # "/sys/devices/platform/host3/session2/target3:0:0/3:0:0:0/block/sda/sda1",
-        # and we want to get the "sda" part of this
-        parent_sys_dir_full = sbd_sys_dir_full[1..sbd_sys_dir_full.rindex("/")-1]
-        parent_disk = "/dev/#{File.basename(parent_sys_dir_full)}"
-        disks = BarclampLibrary::Barclamp::Inventory::Disk.all(node).select { |d| d.name == parent_disk }
-        disk = disks.first
-      end
-    end
-    if disk.nil?
-      raise "Cannot find device #{sbd_device}!"
-    end
-    if disk.claimed? && disk.owner != "sbd"
-      raise "Cannot use #{sbd_device} for SBD: it was claimed for #{disk.owner}!"
-    end
-    unless disk.claim("sbd")
-      raise "Cannot claim #{sbd_device} for SBD!"
-    end
-  end
+  include_recipe "crowbar-pacemaker::sbd"
 
 # Need to add the hostlist param for shared
 when "shared"
