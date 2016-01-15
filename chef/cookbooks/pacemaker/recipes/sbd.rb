5b5fd5711052d9718a57dd8aa80df1a2eaae918b
Vincent Untz <vuntz@suse.com>
2016-01-15 01:05:34 +0100

    pacemaker: Add attribute to help know if a node is remote
    This is useful for the sbd recipe, where we don't want to restart
    corosync on remote nodes.

--- chef/cookbooks/pacemaker/recipes/sbd.rb
+++ chef/cookbooks/pacemaker/recipes/sbd.rb
@@ -77,8 +77,10 @@ if node[:platform_family] == "suse"
     )
     # We want to allocate slots before restarting corosync
     notifies :run, "execute[Allocate SBD slot]", :immediately
-    notifies :restart, "service[#{node[:corosync][:platform][:service_name]}]", :immediately
-    # After restarting corosync, we need to wait for the cluster to be online again
-    notifies :create, "ruby_block[wait for cluster to be online]", :immediately
+    unless node[:pacemaker][:remote]
+      notifies :restart, "service[#{node[:corosync][:platform][:service_name]}]", :immediately
+      # After restarting corosync, we need to wait for the cluster to be online again
+      notifies :create, "ruby_block[wait for cluster to be online]", :immediately
+    end
   end
 end
