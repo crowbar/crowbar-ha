cb6f18cb56e6f79441fe90af2cbf61ae07ea16a5
Vincent Untz <vuntz@suse.com>
2016-02-11 09:35:59 +0100

    crowbar: Fix migrating adding drbd secret
    We were not generating a new secret on migration, resulting in an empty
    secret.

--- chef/data_bags/crowbar/migrate/pacemaker/030_add_drbd_secret.rb
+++ chef/data_bags/crowbar/migrate/pacemaker/030_add_drbd_secret.rb
@@ -1,5 +1,12 @@
 def upgrade(ta, td, a, d)
-  a["drbd"]["shared_secret"] = ta["drbd"]["shared_secret"]
+  # We use a class variable to set the same password in the proposal and in the
+  # role
+  unless defined?(@@drbd_shared_secret)
+    service = ServiceObject.new "fake-logger"
+    @@drbd_shared_secret = service.random_password
+  end
+  a["drbd"]["shared_secret"] = @@drbd_shared_secret
+
   return a, d
 end
 
