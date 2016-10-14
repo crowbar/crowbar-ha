cd0e6ef7f21e25fe6713b689fa87f6c1e54fac2a
Jiří Suchomel <jiri.suchomel@suse.com>
2016-10-14 08:39:27 +0200

    Method for changing the cluster founder to given node
    This is especially needed during the upgrade process when we want
    to make sure that we first chef-client run on upgraded node runs
    on cluster founder (so the pacemaker services can be started there).

--- crowbar_framework/app/models/api/pacemaker.rb
+++ crowbar_framework/app/models/api/pacemaker.rb
@@ -66,6 +66,40 @@ module Api
         ret
       end
 
+      def set_node_as_founder(name)
+        # 1. find the cluster new founder is in
+        new_founder = NodeObject.find_node_by_name(name)
+        if new_founder.nil?
+          Rails.logger.error("Node #{name} not found!")
+          return false
+        end
+        unless new_founder[:pacemaker]
+          Rails.logger.error("Node #{name} does not have pacemaker setup")
+          return false
+        end
+        if new_founder[:pacemaker][:founder]
+          Rails.logger.debug("Node #{name} is already the cluster founder.")
+          return true
+        end
+
+        # 2. find the current cluster founder in the same cluster
+        cluster_env = new_founder[:pacemaker][:config][:environment]
+        old_founder = NodeObject.find(
+          "pacemaker_founder:true AND pacemaker_config_environment:#{cluster_env}"
+        ).first
+
+        if old_founder.nil?
+          Rails.logger.warning("No cluster founder found. Making #{name} the new founder anyway.")
+        else
+          old_founder[:pacemaker][:founder] = false
+          old_founder.save
+        end
+
+        # 3. mark given node as founder
+        new_founder[:pacemaker][:founder] = true
+        new_founder.save
+      end
+
       def repocheck
         Api::Node.repocheck(addon: "ha")
       end
