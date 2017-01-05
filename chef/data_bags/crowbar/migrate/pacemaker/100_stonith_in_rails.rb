bcd66ecf541f733e78a145d707e8f94b63eb7329
Vincent Untz <vuntz@suse.com>
2017-01-05 10:34:57 +0100

    crowbar: Add migration to generate stonith attributes for existing roles
    Up to now, the change for translating crowbar-specific STONITH
    attributes to pacemaker-cookbook-compatible attributes was only working
    for new deployments. We were missing a migration to convert existing
    deployments.

---
+++ chef/data_bags/crowbar/migrate/pacemaker/100_stonith_in_rails.rb
@@ -0,0 +1,23 @@
+def upgrade(ta, td, a, d)
+  # are we a role? If no, nothing to do.
+  #  - crowbar-committing is always true for roles, and generally false for
+  #    proposals (except when applying)
+  #  - the pacemaker barclamp adds an required_post_chef_calls attribute to the
+  #    deployment part of the role
+  return [a, d] unless d["crowbar-committing"] && d.key?("required_post_chef_calls")
+
+  members = d["elements"]["pacemaker-cluster-member"] || []
+  member_nodes = members.map { |n| NodeObject.find_node_by_name n }
+  remotes = d["elements"]["pacemaker-remote"] || []
+  remote_nodes = remotes.map { |n| NodeObject.find_node_by_name n }
+
+  service = PacemakerService.new Rails.logger
+  service.prepare_stonith_attributes(a, remote_nodes, member_nodes, remotes, members)
+
+  return a, d
+end
+
+def downgrade(ta, td, a, d)
+  # nothing; it doesn't hurt to keep the changes
+  return a, d
+end
