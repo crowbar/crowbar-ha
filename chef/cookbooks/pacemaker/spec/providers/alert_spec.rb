c32b532e433566891163e973605834ab498a13a3
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2017-12-05 13:50:46 +0100

    pacemaker: Alert resource
    Pacemaker supports alert feature which can be used to attach scripts
    to various cluster events. This commit exposes basic alerts to chef as
    resources. Creating, updating and deleting is supported.

---
+++ chef/cookbooks/pacemaker/spec/providers/alert_spec.rb
@@ -0,0 +1,51 @@
+require "spec_helper"
+
+require_relative "../helpers/non_runnable_resource"
+require_relative "../fixtures/alert"
+
+describe "Chef::Provider::PacemakerAlert" do
+  # for use inside examples:
+  let(:fixture) { Chef::RSpec::Pacemaker::Config::ALERT.dup }
+  # for use outside examples (e.g. when invoking shared_examples)
+  fixture = Chef::RSpec::Pacemaker::Config::ALERT.dup
+
+  def lwrp_name
+    "alert"
+  end
+
+  include_context "a Pacemaker LWRP with artificially constructed resource"
+
+  before(:each) do
+    @resource.handler fixture.handler.dup
+    @resource.receiver fixture.receiver.dup
+  end
+
+  def cib_object_class
+    Pacemaker::Alert
+  end
+
+  shared_examples "an updateable resource" do
+    include Chef::RSpec::Pacemaker::CIBObject
+
+    it "should modify the alert if the resource is changed" do
+      expected = fixture.dup
+      expected.handler = "handler2.sh"
+      expected_configure_cmd_args = [expected.reconfigure_command]
+      test_modify(expected_configure_cmd_args) do
+        @resource.handler expected.handler
+      end
+    end
+  end
+
+  describe ":create action" do
+    let(:action) { :create }
+    it_should_behave_like "an updateable resource"
+  end
+
+  describe ":update action" do
+    let(:action) { :update }
+    it_should_behave_like "an updateable resource"
+  end
+
+  it_should_behave_like "a non-runnable resource", fixture
+end
