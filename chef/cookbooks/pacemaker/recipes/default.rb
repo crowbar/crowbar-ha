0239c5e1ba749a29a804a4ac7dee96c6d471144d
Adam Spiers <aspiers@suse.com>
2018-06-21 12:58:26 +0100

    improve debug of cluster ready check
    If the cluster is not ready yet, make it clear in normal logs
    which check is failing.

--- chef/cookbooks/pacemaker/recipes/default.rb
+++ chef/cookbooks/pacemaker/recipes/default.rb
@@ -80,10 +80,20 @@ ruby_block "wait for cluster to be online" do
             next
           end
           crm_mon = crm_mon_cmd.stdout
-          break if crm_mon.include?("#{cluster_size} nodes configured") &&
-              crm_mon.include?("Online:") &&
-              crm_names.sort == nodes_names.sort
-          Chef::Log.debug("cluster not online yet")
+          ready = true
+          if !crm_mon.include?("#{cluster_size} nodes configured")
+            ready = false
+            Chef::Log.warn("cluster doesn't have #{cluster_size} nodes configured yet")
+          elsif !crm_mon.include?("Online:")
+            ready = false
+            Chef::Log.warn("cluster doesn't have nodes online yet")
+          elsif crm_names.sort != nodes_names.sort
+            ready = false
+            Chef::Log.warn("crm_node -l listed nodes #{crm_names.sort}, " \
+                           "not #{nodes_names.sort} as expected")
+          end
+          break if ready
+          Chef::Log.warn("cluster not online yet")
           sleep(5)
         end
       end
